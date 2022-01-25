ae42719082652c77e7ec5533bb2c0941e4687cb9
Faisal Syed <fas2001@yum.com>
2022-01-25 12:34:12 -0800

    Initial commit

---
+++ src/Project1.java
@@ -0,0 +1,11 @@
+public class Project1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is program 1!");
+
+        // Run our main programs in here
+
+        // Call any/all testing routines
+    }
+}
